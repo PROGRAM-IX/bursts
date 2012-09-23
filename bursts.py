44915dc6e846fcfaf682c1a2bfa026b6f7dc9725
James Heslin <program.ix.j@gmail.com>
2012-09-23 18:22:18 +0100

    + Added random movement of bursts + Added random colour changing to bursts ~ Changed burst lifetime to 30 ticks

--- bursts.py
+++ bursts.py
@@ -27,9 +27,18 @@ class Burst():
         self.particles.append(Particle(self.x, self.y, 5, self.colour))
     def update(self):
         self.particles.append(Particle(random.randint(1, 30) + self.x, 
-            random.randint(1, 30) + self.y, 5, self.colour))
-                
-
+            random.randint(1, 30) + self.y, 5, self.colour))    
+        self.y += random.randint(-100, 100)
+        self.x += random.randint(-100, 100)
+        modR = random.randint(-40, 40)
+        modG = random.randint(-40, 40)
+        modB = random.randint(-40, 40)
+        if self.colour.r + modR <= 255 and self.colour.r + modR >=0:
+            self.colour.r += modR
+        if self.colour.g + modG <= 255 and self.colour.g + modG >=0:
+            self.colour.g += modG
+        if self.colour.b + modB <= 255 and self.colour.b + modB >=0:
+            self.colour.b += modB
     def draw(self):
         global screen
         for p in self.particles:
@@ -49,7 +58,7 @@ class Canvas():
         global screen, notes, channels
         #print "Updating canvas"
         for b in self.bursts:
-            if b.count < 15:
+            if b.count < 30:
                 b.update()
                 b.draw()
                 b.count += 1
@@ -62,48 +71,48 @@ class Canvas():
                 if e.key == K_q:
                     raise SystemExit
                 if e.key == K_a:
-                    self.bursts.append(Burst(30, 80, (0, 0, 128), 0, 
+                    self.bursts.append(Burst(30, 80, pygame.Color(0, 0, 128), 0, 
                         notes['c']))
                     channels['c_chan'].play(notes['c'])
                     #print "C"
 
                 elif e.key == K_s:
-                    self.bursts.append(Burst(110, 80, (0, 0, 255), 0, 
+                    self.bursts.append(Burst(110, 80, pygame.Color(0, 0, 255), 0, 
                         notes['d']))
                     channels['d_chan'].play(notes['d'])
                     #print "D"
                 elif e.key == K_d:
-                    self.bursts.append(Burst(190, 80, (0, 128, 0), 0, 
+                    self.bursts.append(Burst(190, 80, pygame.Color(0, 128, 0), 0, 
                         notes['e']))
                     channels['e_chan'].play(notes['e'])
                     #print "E"
 
                 elif e.key == K_f:
-                    self.bursts.append(Burst(270, 80, (0, 128, 128), 0, 
+                    self.bursts.append(Burst(270, 80, pygame.Color(0, 128, 128), 0, 
                         notes['f']))
                     channels['f_chan'].play(notes['f'])
                     #print "F"
 
                 elif e.key == K_j:
-                    self.bursts.append(Burst(350, 80, (0, 255, 0), 0,
+                    self.bursts.append(Burst(350, 80, pygame.Color(0, 255, 0), 0,
                         notes['g']))
                     channels['g_chan'].play(notes['g'])
                     #print "G"
 
                 elif e.key == K_k:
-                    self.bursts.append(Burst(430, 80, (0, 255, 128), 0, 
+                    self.bursts.append(Burst(430, 80, pygame.Color(0, 255, 128), 0, 
                         notes['a']))
                     channels['a_chan'].play(notes['a'])
                     #print "A"
 
                 elif e.key == K_l:
-                    self.bursts.append(Burst(510, 80, (0, 255, 255), 0, 
+                    self.bursts.append(Burst(510, 80, pygame.Color(0, 255, 255), 0, 
                         notes['b']))
                     channels['b_chan'].play(notes['b'])
                     #print "B"
 
                 elif e.key == K_SEMICOLON:
-                    self.bursts.append(Burst(590, 80, (128, 0, 0), 0, 
+                    self.bursts.append(Burst(590, 80, pygame.Color(128, 0, 0), 0, 
                         notes['hc']))
                     channels['hc_chan'].play(notes['hc'])
                     #print "HC"
